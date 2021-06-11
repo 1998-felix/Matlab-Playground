edc06c654c0ca25ca8400354bcc5d96549914d5e
Felix Gateru <felix.gateru@gmail.com>
2021-06-11 05:36:18 +0300

    closes #2

--- ode/main.m
+++ ode/main.m
@@ -1,10 +1,22 @@
+% input commands to obtain variables = Tr
+start = true;
 fprintf("Program to solve equations of the form ay'' + by' + cy = 0 \n");
-fprintf("Please input your differential equation \n");
-a = input("Please input the first co-efficient, a :");
-b = input("Please input the second co-efficient, b :");
-c = input("Please input the third co-efficient, c :");
-y_0 = input("Please input the first initial condition, y(o) :");
-y_1 = input("Please input the second initial condition, y'(o) :");
+while start
+    fprintf("Please input your differential equation \n");
+    a = input("Please input the first co-efficient, a :");
+    b = input("Please input the second co-efficient, b :");
+    c = input("Please input the third co-efficient, c :");
+    y_0 = input("Please input the first initial condition, y(o) :");
+    y_1 = input("Please input the second initial condition, y'(o) :");
+    fprintf("The equation you have entered is %d y'' + %dy' + %d y == 0" , a ,b ,c);
+    state = input("Press one to continue:");
+    if state == 1
+        start = false;
+    else
+        start=true;
+    end
+      
+end
 
 r = my_root(a, b , c);
 syms y(x) C1 C2
